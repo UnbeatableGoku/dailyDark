6fd6fbb053d7c06aaa8705e609612c63f82def8d
prathmeshgoodboy11@gmail.com <prathmeshgoodboy11@gmail.com>
2022-02-14 18:42:35 +0530

    Stardus

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
 const express = require("express");
 const https = require("https")
+const ejs = require("ejs")
+const bodyParser = require("body-parser")
+var joke2 = "";
 const app = express();
-const jokef = require(__dirname + "/joke1.js")
+
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(__dirname + "/public"))
+
 
 app.get("/", function(req, res) {
-        let joke1 = jokef.getjoke1();
-        // res.write(joke1);
-        // res.sendFile(__dirname + "/index.html")
-        console.log(joke1);
-        res.send(joke1)
+    let url = "https://v2.jokeapi.dev/joke/Dark?type=twopart";
 
+    https.get(url, function(responce) {
+        responce.on("data", function(data) {
+            const jokeObj = JSON.parse(data)
+            var joke1 = jokeObj.setup
+            res.render(__dirname + "/index.ejs", { joke1: joke1 })
+            console.log(jokeObj.delivery);
+            joke2 = jokeObj.delivery;
+        })
     })
-    // app.post("/", function(req, res) {
-    //     console.log("done");
-    // })
+})
 
+app.post("/", function(req, res) {
+    res.render(__dirname + "/answer.ejs", { joke2: joke2 })
+})
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("we connected");
 })
+
+
+
+
+
+
+
+
+
+
+
+//https://v2.jokeapi.dev/joke/Dark
